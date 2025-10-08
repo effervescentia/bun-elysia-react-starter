a2cc25e44a963332142d25b7e613044fc9c4549b
Ben Teichman <ben@voiceflow.com>
2025-10-08 13:53:54 -0400

    feat: add init script to setup project

--- plopfile.ts
+++ plopfile.ts
@@ -4,17 +4,60 @@ import type { NodePlopAPI } from 'plop';
 export default function (plop: NodePlopAPI) {
   usePlugins(plop);
 
-  // REMOVE_AFTER_THIS_POINT
+  // REMOVE_ON_INIT
   plop.setGenerator('init', {
     description: 'initialize repository by rewriting key files',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'project name',
+      },
+    ],
     actions: [
       {
         type: 'modify',
         path: 'plopfile.ts',
-        pattern: /\/\/ REMOVE_AFTER_THIS_POINT.*\/\/ REMOVE_BEFORE_THIS_POINT/g,
+        pattern: /\/\/ REMOVE_ON_INIT(\n.*)+REMOVE_ON_INIT/m,
         template: '',
+      },
+      {
+        type: 'modify',
+        path: 'README.md',
+        pattern: /^.*$/m,
+        template: '# {{kebabCase name}}',
+      },
+      {
+        type: 'modify',
+        path: 'package.json',
+        pattern: /"app"/,
+        template: '"{{kebabCase name}}"',
+      },
+      {
+        type: 'modify',
+        path: 'package.json',
+        pattern: /^\s+"init":.*$/,
+        template: '',
+      },
+      {
+        type: 'modify',
+        path: 'apps/api/package.json',
+        pattern: /"@app\/api"/,
+        template: '"@{{kebabCase name}}/api"',
+      },
+      {
+        type: 'modify',
+        path: 'apps/web/package.json',
+        pattern: /"@app\/api"/,
+        template: '"@{{kebabCase name}}/api"',
+      },
+      {
+        type: 'modify',
+        path: 'apps/web/package.json',
+        pattern: /"@app\/web"/,
+        template: '"@{{kebabCase name}}/web"',
       },
     ],
   });
-  // REMOVE_BEFORE_THIS_POINT
+  // REMOVE_ON_INIT
 }
