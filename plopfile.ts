e43812ff34e2c5d258bb87ce2bdc1281fe8ed90a
Ben Teichman <ben@voiceflow.com>
2025-10-08 13:35:53 -0400

    feat: init plop command

--- plopfile.ts
+++ plopfile.ts
@@ -3,4 +3,18 @@ import type { NodePlopAPI } from 'plop';
 
 export default function (plop: NodePlopAPI) {
   usePlugins(plop);
+
+  // REMOVE_AFTER_THIS_POINT
+  plop.setGenerator('init', {
+    description: 'initialize repository by rewriting key files',
+    actions: [
+      {
+        type: 'modify',
+        path: 'plopfile.ts',
+        pattern: /\/\/ REMOVE_AFTER_THIS_POINT.*\/\/ REMOVE_BEFORE_THIS_POINT/g,
+        template: '',
+      },
+    ],
+  });
+  // REMOVE_BEFORE_THIS_POINT
 }
