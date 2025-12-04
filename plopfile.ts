b31906769bf05435f18e920de832df3512f33169
Ben Teichman <ben@voiceflow.com>
2025-12-04 14:19:39 -0500

    feat: add skaffold, docker, k8s (#2)

--- plopfile.ts
+++ plopfile.ts
@@ -1,3 +1,4 @@
+import fs from 'node:fs/promises';
 import { usePlugins } from '@bltx/plop';
 import type { NodePlopAPI } from 'plop';
 
@@ -7,13 +8,26 @@ export default function (plop: NodePlopAPI) {
   // REMOVE_ON_INIT
   plop.setGenerator('init', {
     description: 'initialize repository by rewriting key files',
-    prompts: [
-      {
-        type: 'input',
-        name: 'name',
-        message: 'project name',
-      },
-    ],
+    prompts: async (inquirer) => {
+      const { name } = await inquirer.prompt<{ name: string }>([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'project name',
+        },
+      ]);
+
+      const { envPrefix } = await inquirer.prompt<{ envPrefix: string }>([
+        {
+          type: 'input',
+          name: 'envPrefix',
+          message: 'env variable prefix',
+          default: name.toUpperCase(),
+        },
+      ]);
+
+      return { name, envPrefix };
+    },
     actions: [
       {
         type: 'modify',
@@ -59,9 +73,44 @@ export default function (plop: NodePlopAPI) {
       },
       {
         type: 'modify',
+        path: 'apps/web/src/app/app.env.ts',
+        pattern: /__app__/g,
+        template: '{{snakeCase envPrefix}}',
+      },
+      {
+        type: 'modify',
+        path: 'tests/e2e/package.json',
+        pattern: /"@app\/test-e2e"/,
+        template: '"@{{kebabCase name}}/test-e2e"',
+      },
+      {
+        type: 'modify',
+        path: 'tests/e2e/playwright.config.mts',
+        pattern: /__app__/g,
+        template: '{{kebabCase name}}',
+      },
+      {
+        type: 'modify',
+        path: 'skaffold.yaml',
+        pattern: /__app__/g,
+        template: '{{kebabCase name}}',
+      },
+      {
+        type: 'modify',
         path: 'README.md',
         pattern: /^.*$/s,
         template: '# {{kebabCase name}}\n',
+      },
+      {
+        type: 'addMany',
+        destination: '.',
+        templateFiles: '.plop/init',
+        base: '.plop/init',
+      },
+      async () => {
+        await fs.rm('.plop/init', { recursive: true, force: true });
+
+        return 'deleted .plop/init';
       },
     ],
   });
